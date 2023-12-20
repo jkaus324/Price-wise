09918087cb00f5a0a2521f8f552d45552d4986ec
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2023-12-21 02:13:57 +0530

    Almost done
    some bugs left to checked out rest website full functional, email not able to sent

--- price-wise/app/products/[id]/page.tsx
+++ price-wise/app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
 import { getProductById, getSimilarProducts } from "@/lib/actions"
@@ -148,7 +149,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                             />
                         </div>
                     </div>
-                    Modal
+                    <Modal productId={id} />
                 </div>
             </div>
             <div className="flex flex-col gap-16">
